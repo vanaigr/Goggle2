542bff4974ec429f49f5ceec7ba826e381b82c09
vanaigr <vanaigranov@gmail.com>
2024-04-13 16:24:43 -0500

    test HTTP server in c

--- src/main.c
+++ src/main.c
@@ -1,66 +1,145 @@
-#include<stdio.h>
+//#include<windows.h>
+//#include<wininet.h>
 #include<winsock2.h>
-#include<stdint.h>
+#include<stdio.h>
 
+#include<stdint.h>
+#include<stdbool.h>
+
+//#pragma comment(lib, "wininet.lib")
 #pragma comment(lib, "ws2_32.lib")
 
-int const RESP_SIZE = 20000;
+#define ARR_SIZE(arr) (sizeof((arr)) / sizeof((arr)[0]))
 
 int main(void) {
-    WSADATA wsaData;
+WSADATA wsa;
     SOCKET s;
-    struct sockaddr_in server;
-    char *message, server_reply[RESP_SIZE];
-    int recv_size;
 
-    // Initialize Winsock
-    if (WSAStartup(MAKEWORD(2,2), &wsaData) != 0) {
-        printf("Failed to initialize Winsock.\n");
+    printf("\nInitialising Winsock...");
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+        printf("Failed. Error Code : %d", WSAGetLastError());
         return 1;
     }
 
-    // Create socket
-    if ((s = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
-        printf("Could not create socket.\n");
-        return 1;
+    printf("Initialised.\n");
+
+    // Create a socket
+    if ((s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == INVALID_SOCKET) {
+        printf("Could not create socket : %d", WSAGetLastError());
     }
 
     printf("Socket created.\n");
 
-    uint32_t addr = (142u) | (251u << 8) | (46u << 16) | (174u << 24);
-
-    // Set up the server address
-    server.sin_addr.s_addr = addr;
+    // Prepare the sockaddr_in structure
+    struct sockaddr_in server;
     server.sin_family = AF_INET;
-    server.sin_port = htons(80); // HTTP port
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(1619);
 
-    // Connect to server
-    if (connect(s, (struct sockaddr *)&server, sizeof(server)) < 0) {
-        printf("Connect failed.\n");
+    // Bind
+    if (bind(s, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR) {
+        printf("Bind failed with error code : %d", WSAGetLastError());
         return 1;
     }
+    printf("Bind done\n");
 
-    printf("Connected.\n");
+    static const char page_msg[]
+        =
+        "HTTP/1.0 200 OK\r\n"
+        "Content-type: text/html\r\n"
+        //"Content-length: 96\r\n"
+        "\r\n"
+        "<!DOCTYPE html><html><head><title>Test!</title></head><body><h1>Hello, World!</h1></body></html>";
+    int const page_c = ARR_SIZE(page_msg);
 
-    // Send HTTP request
-    message = "GET /search?q=bob HTTP/1.1\r\nHost: www.google.com\r\n\r\n";
-    if (send(s, message, strlen(message), 0) < 0) {
-        printf("Send failed.\n");
-        return 1;
+    static char const not_found_msg[] = "HTTP/1.0 404 Not Found\r\n\r\n";
+    int const not_found_c = ARR_SIZE(not_found_msg);
+    static char const not_impl_msg[] = "HTTP/1.0 501 Not Implemented\r\n\r\n";
+    int const not_impl_c = ARR_SIZE(not_impl_msg);
+
+    static char client_request[2048];
+
+    listen(s, 3);
+
+    while(true) {
+        printf("Waiting for incoming connections...\n");
+
+        SOCKET conn_socket = accept(s, NULL, NULL);
+        if (conn_socket == INVALID_SOCKET) {
+            printf("Accept failed with error code : %d", WSAGetLastError());
+            continue;
+        }
+
+        int received = recv(conn_socket, client_request, 2047, 0);
+        printf("Received %d bytes: `%s`", received, client_request);
+        client_request[received] = '\0';
+
+        static char const get[] = "GET";
+        static char const search[] = "/search?q=";
+
+        char const *msg;
+        int msg_c;
+        if(received < 3 || memcmp(client_request, get, 3) != 0) {
+            printf("Response not Implemented\n");
+            msg = not_impl_msg;
+            msg_c = not_impl_c;
+        }
+        else if(received < 14 || memcmp(client_request + 4, search, 10) != 0) {
+            printf("Response is asking for something oth\n");
+            msg = not_found_msg;
+            msg_c = not_impl_c;
+        }
+        else {
+            printf("Returning main page\n");
+            msg = page_msg;
+            msg_c = page_c;
+        }
+
+        printf("Connection accepted\n");
+        int size = send(conn_socket, msg, msg_c, 0);
+        shutdown(conn_socket, SB_BOTH);
+        printf("Sent %d of %d\n", size, msg_c);
+        closesocket(conn_socket);
     }
-    printf("HTTP request sent.\n");
-
-    // Receive a response from the server
-    if ((recv_size = recv(s, server_reply, RESP_SIZE, 0)) == SOCKET_ERROR) {
-        printf("Receive failed.\n");
-    }
-
-    // Print the response
-    printf("Server reply:\n");
-    printf("%.*s", recv_size, server_reply);
 
     closesocket(s);
     WSACleanup();
 
     return 0;
 }
+
+#if 0
+    HINTERNET hInternet = NULL;
+    HINTERNET hConnect = NULL;
+    HINTERNET hRequest = NULL;
+
+    // Initialize WinINet
+    hInternet = InternetOpenA("HTTP GET", INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, 0);
+    if (!hInternet) {
+        printf("Failed to initialize WinINet: %lu\n", GetLastError());
+        return 1;
+    }
+
+    // Connect to the server
+    hConnect = InternetOpenUrlA(hInternet, "https://google.com/search?q=bob", NULL, 0, INTERNET_FLAG_SECURE, 0);
+    if (!hConnect) {
+        printf("Failed to connect: %lu\n", GetLastError());
+        InternetCloseHandle(hInternet);
+        return 1;
+    }
+
+    // Send the GET request
+    char buffer[4096];
+    DWORD bytesRead;
+    while (InternetReadFile(hConnect, buffer, sizeof(buffer), &bytesRead) && bytesRead > 0) {
+        // Output received data
+        fwrite(buffer, 1, bytesRead, stdout);
+    }
+
+    // Cleanup
+    InternetCloseHandle(hConnect);
+    InternetCloseHandle(hInternet);
+
+    return 0;
+}
+#endif
